c8650324b58b588ac7f9251f4f668107467f1c27
Roberto Leon <roberto.leonjara@gmail.com>
2025-09-22 20:44:58 -0500

    Se agregó la descripción general del proyecto

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let maximoValor = 10;
+
+let posicionAleatoria = Math.floor(Math.random()*maximoValor)+1;
+
+let listaMisAmigos = [];
+
+console.log(posicionAleatoria);
+
+function agregarAmigo() {
+    let nombresDeAmigos = document.getElementById('amigo').value;
+    listaMisAmigos.push(nombresDeAmigos);
+    
+    console.log(nombresDeAmigos);
+    console.log(listaMisAmigos);
+    limpiarImput();
+}
+
+function limpiarImput() {
+    document.querySelector('#amigo').value = '';
+}
+
+function sortearAmigo() {
+    
+}
